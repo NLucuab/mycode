975a37146b6d462a4e7f29d25405b26b2cacb71f
NLucuab <nlucuab18@gmail.com>
2023-02-21 18:33:21 +0000

    join and split

---
+++ challenge.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+ name = input("Enter a name: ")
+ day_of_week = input("Enter a day of the week: ")
+ greeting = "Hello, {name}! Happy {day_of_week}!".format(name=name, day_of_week=day_of_week)
+ print(greeting)
