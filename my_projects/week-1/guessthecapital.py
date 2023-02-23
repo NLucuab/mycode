1f644f87be73388decb7919802efe6529d8a8f4a
NLucuab <nlucuab18@gmail.com>
2023-02-23 16:15:33 +0000

    Minor fixes to game/print outs

--- my_projects/week-1/guessthecapital.py
+++ my_projects/week-1/guessthecapital.py
@@ -33,14 +33,33 @@ def guess_the_captial():
     have a while loop that will keep going until a counter (equal to the len(countries))
     reaches 0. 
         * inside that while loop, ask print out which country the user will be guessing the capital for
-        * keep a variable, starting at 0, to be the idx 
+        * keep a variable, starting at 0, to be the idx tracker for the countries list
+        * increment the country_idx counter after every guess
+        * keep a variable that keeps track of correct guesses
+        * increment the correct guess counter when a guess is correct
     """
 
-    print("Welcome to Guess That Capital!")
-    count = 10
+    print("Welcome to Guess That Capital! \n")
+    country_count = 10
+    correct_guess_count = 0
+    country_idx = 0
 
-    while count > 0:
-        country_idx = 0
-        print(f"What is the capital of {capitals[country_idx]}?")
-        print("\n")
-        captial_guess = input("")
+    while country_count > 0:
+        print(f"What is the capital of {countries[country_idx]}?")
+        captial_guess = input("Enter your guess here: ").lower()
+
+        if captial_guess == countries_and_capitals[countries[country_idx]].lower():
+            print("You are correct! \n")
+            correct_guess_count += 1
+
+        else:
+            print("Nice try, but not quite correct there. \n")
+            
+        country_count -= 1
+        country_idx += 1
+        
+    
+    print("Thank you for playing the game! \n")
+    print(f"You scored: {correct_guess_count}/10")
+
+guess_the_captial()
