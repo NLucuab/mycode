05bf345cdcacb8869e7faffa5f0a7053030ff8e8
NLucuab <nlucuab18@gmail.com>
2023-02-26 09:07:43 +0000

    updated week-1 project and included uncommited files

--- my_projects/week-1/guessthecapital.py
+++ my_projects/week-1/guessthecapital.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import os
+from time import sleep
 
 def main():
     # TODO: Refactor code to only deal with dictionary, rather than both a list & dictionary 
@@ -30,45 +32,75 @@ def main():
         "New Zealand" : "Wellington"
     }
 
-    # have a while loop that will keep going until a counter (equal to the len(countries))
-    # reaches 0. 
-    #     * inside that while loop, ask print out which country the user will be guessing the capital for
-    #     * keep a variable, starting at 0, to be the idx tracker for the countries list
-    #     * increment the country_idx counter after every guess
-    #     * keep a variable that keeps track of correct guesses
-    #     * increment the correct guess counter when a guess is correct
-
     print("Welcome to Guess That Capital! \n")
+
+    print( 
+    """
+    Just some rules for ya:
+    1) Have fun!
+    2) Remember: It's just a game!
+    3) Enjoy~ 
+    """
+    )
+
+    username = input("May I know who's playing?: ")
+    print("\n")
+    print(f"Welcome, {username}! \n")
+
+
     country_count = 10
     correct_guess_count = 0
     country_idx = 0
+    guess_count = 5
 
     while country_count > 0:
-        print(f"What is the capital of {countries[country_idx]}?")
-        captial_guess = input("Enter your guess here: ").lower()
 
-        if captial_guess == countries_and_capitals[countries[country_idx]].lower():
+        print(f"What is the capital of {countries[country_idx]}?")
+
+        print(f"Guesses left: {guess_count} \n")
+
+        capital_guess = input("Enter your guess here: ").lower()
+
+        if capital_guess == countries_and_capitals[countries[country_idx]].lower():
             print("You are correct! \n")
             correct_guess_count += 1
+            country_idx += 1
+            country_count -= 1
+            sleep(2)
+            os.system('clear')
+            guess_count = 5
 
-        else:
-            print("Nice try, but not quite correct there. \n")
-            
-        country_count -= 1
-        country_idx += 1
+        elif capital_guess != countries_and_capitals[countries[country_idx]].lower() and guess_count > 1:
+            print("Nice try, but not quite. Let's try one more time. \n")
+            guess_count -= 1
+            sleep(2)
+            os.system('clear')
         
-    
-    print("Thank you for playing the game! \n")
-    print(f"You scored: {correct_guess_count}/10")
+        elif capital_guess != countries_and_capitals[countries[country_idx]].lower() and guess_count == 1:
+            print("Nice try, last chance! \n")
+            guess_count -= 1
+            sleep(2)
+            os.system('clear') 
+            
+        elif guess_count == 0:
+            print(f"Nice try! The correct answer was: {countries_and_capitals[countries[country_idx]]}. \n")
+            country_idx += 1
+            country_count -= 1
+            sleep(2)
+            os.system('clear')
+            guess_count = 5
+        
+    print(f"You scored: {correct_guess_count}/10\n")
+    print(f"Thanks for playing, {username}!")
 
 if __name__ == "__main__":
     main()
 
 
 # stretch goals:
-# * get the user's name and use that throughout the game :) 
-# * Randomize the selection of country, rather than going down the line
-# * Consolidate access to just the dictionary, rather than both the list & dictionary
-# * Give hints, if the user gets it wrong (set # of guesses => display that as well)
-# * print out the amount of guesses (per questions) that the user made before they got it correct
-# * add in invalid entry handling(entering a int, float, etc...)
+# * get the user's name and use that throughout the game :) DONE
+# * Randomize the selection of country, rather than going down the line >> opted to skip
+# * Consolidate access to just the dictionary, rather than both the list & dictionary >> will come back and do this!
+# * Give hints, if the user gets it wrong (set # of guesses => display that as well) >> set guess amount to 5 and displayed that!
+# * print out the amount of guesses (per questions) that the user made before they got it correct DONE
+# * add in invalid entry handling(entering a int, float, etc...) >>> Just made it so that it's a wrong answer!
